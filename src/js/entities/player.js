7542ea4dc79483b02f02797971985826c232ceb9
LeMustelide <marcvirgili.contact@gmail.com>
2025-05-03 23:38:40 +0200

    Update player and map configuration: modify player dimensions, enhance player rendering, and clean up play screen logic.

--- src/js/entities/player.js
+++ src/js/entities/player.js
@@ -1,57 +1,62 @@
-import * as me from 'melonjs';
-import game from './../game.js';
+import * as me from "melonjs";
+import game from "./../game.js";
 
 class PlayerUnit extends me.Entity {
     constructor(x, y, settings) {
-        settings.width = 32;  // Définir la largeur du joueur
-        settings.height = 32; // Définir la hauteur du joueur
-
-        // Charger l'image du joueur
-        settings.image = "character";
-
         // Appeler le constructeur parent
         super(x, y, settings);
 
         // set a "player object" type
         this.body.collisionType = me.collision.types.PLAYER_OBJECT;
-        
+
         // Défini le joueur comme une entité qui peut être déplacée
         this.body.setMaxVelocity(3, 15);
         this.body.gravity = 0;
+        this.body.gravityScale = 0; // Set gravity scale to 0.5 for a slower fall
 
         // enable keyboard
-        me.input.bindKey(me.input.KEY.LEFT,  "left");
+        me.input.bindKey(me.input.KEY.LEFT, "left");
         me.input.bindKey(me.input.KEY.RIGHT, "right");
-        me.input.bindKey(me.input.KEY.X,     "jump", true);
-        me.input.bindKey(me.input.KEY.UP,    "jump", true);
+        me.input.bindKey(me.input.KEY.X, "jump", true);
+        me.input.bindKey(me.input.KEY.UP, "jump", true);
         me.input.bindKey(me.input.KEY.SPACE, "jump", true);
-        me.input.bindKey(me.input.KEY.DOWN,  "down");
+        me.input.bindKey(me.input.KEY.DOWN, "down");
 
-        me.input.bindKey(me.input.KEY.A,     "left");
-        me.input.bindKey(me.input.KEY.D,     "right");
-        me.input.bindKey(me.input.KEY.W,     "jump", true);
-        me.input.bindKey(me.input.KEY.S,     "down");
+        me.input.bindKey(me.input.KEY.A, "left");
+        me.input.bindKey(me.input.KEY.D, "right");
+        me.input.bindKey(me.input.KEY.W, "jump", true);
+        me.input.bindKey(me.input.KEY.S, "down");
 
+        // set a renderable
+        this.renderable = new me.Sprite(0, 0, {
+            image: me.loader.getImage("character"),
+            framewidth: 20, // Set the frame width of the sprite
+            frameheight: 20, // Set the frame height of the sprite
+        });
+
+        this.renderable.addAnimation("stand", [0], 100);
+
+        // set as default
+        this.renderable.setCurrentAnimation("stand");
         // set the viewport to follow this renderable on both axis, and enable damping
         me.game.viewport.follow(this, me.game.viewport.AXIS.BOTH, 0.1);
 
         this.anchorPoint.set(0.5, 1.0); // Ancre le joueur au bas de l'image
-
     }
 
     update(dt) {
         // Ajoute la logique pour le mouvement ou autres interactions
         if (me.input.isKeyPressed("left")) {
-            this.body.vel.x -= this.body.accel.x * dt / 1000;
+            this.body.vel.x -= (this.body.accel.x * dt) / 1000;
         }
         if (me.input.isKeyPressed("right")) {
-        this.body.vel.x += this.body.accel.x * dt / 1000;
+            this.body.vel.x += (this.body.accel.x * dt) / 1000;
         }
         if (me.input.isKeyPressed("up")) {
-        this.body.vel.y -= this.body.accel.y * dt / 1000;
+            this.body.vel.y -= (this.body.accel.y * dt) / 1000;
         }
         if (me.input.isKeyPressed("down")) {
-        this.body.vel.y += this.body.accel.y * dt / 1000;
+            this.body.vel.y += (this.body.accel.y * dt) / 1000;
         }
 
         // Appelle la méthode update du parent pour gérer les collisions et autres
