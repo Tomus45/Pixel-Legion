dd57eafab762ea803926149920e0b5b70d8ce887
LeMustelide <marcvirgili.contact@gmail.com>
2025-05-04 00:42:01 +0200

    Add character image and update player dimensions; implement click-to-move functionality

--- src/js/entities/player.js
+++ src/js/entities/player.js
@@ -14,19 +14,6 @@ class PlayerUnit extends me.Entity {
         this.body.gravity = 0;
         this.body.gravityScale = 0; // Set gravity scale to 0.5 for a slower fall
 
-        // enable keyboard
-        me.input.bindKey(me.input.KEY.LEFT, "left");
-        me.input.bindKey(me.input.KEY.RIGHT, "right");
-        me.input.bindKey(me.input.KEY.X, "jump", true);
-        me.input.bindKey(me.input.KEY.UP, "jump", true);
-        me.input.bindKey(me.input.KEY.SPACE, "jump", true);
-        me.input.bindKey(me.input.KEY.DOWN, "down");
-
-        me.input.bindKey(me.input.KEY.A, "left");
-        me.input.bindKey(me.input.KEY.D, "right");
-        me.input.bindKey(me.input.KEY.W, "jump", true);
-        me.input.bindKey(me.input.KEY.S, "down");
-
         // set a renderable
         this.renderable = new me.Sprite(0, 0, {
             image: me.loader.getImage("character"),
@@ -39,9 +26,36 @@ class PlayerUnit extends me.Entity {
         // set as default
         this.renderable.setCurrentAnimation("stand");
         // set the viewport to follow this renderable on both axis, and enable damping
-        me.game.viewport.follow(this, me.game.viewport.AXIS.BOTH, 0.1);
+        // me.game.viewport.follow(this, me.game.viewport.AXIS.BOTH, 0.1);
+        // set the viewport fix position
+        me.game.viewport.centerOn(0, 800);
 
         this.anchorPoint.set(0.5, 1.0); // Ancre le joueur au bas de l'image
+
+        // Variable pour suivre si le joueur est sélectionné
+        this.isSelected = false;
+
+        // Écouteur d'événements pour la sélection du joueur
+        // me.input.bindPointer(me.input.pointer.LEFT, "leftClick", true);
+
+        // Écouteur de clic
+        me.input.registerPointerEvent(
+            "pointerdown",
+            me.game.viewport,
+            (event) => {
+                console.log("Clic détecté !");
+                const worldX = event.gameLocalX - this.width / 2; // Ajuster la position pour centrer le joueur
+                const worldY = event.gameLocalY - this.height; // Ajuster la position pour centrer le joueur
+                console.log("clic", event);
+
+                this.targetPos = { x: worldX, y: worldY };
+            }
+        );
+
+        // Écouteur d'événements pour le clic gauche de la souris
+
+        // Position cible initiale
+        this.targetPosition = null;
     }
 
     update(dt) {
@@ -57,6 +71,23 @@ class PlayerUnit extends me.Entity {
         }
         if (me.input.isKeyPressed("down")) {
             this.body.vel.y += (this.body.accel.y * dt) / 1000;
+        }
+        if (this.targetPos) {
+            const dx = this.targetPos.x - this.pos.x;
+            const dy = this.targetPos.y - this.pos.y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+
+            // console.log("Distance to target:", distance);
+            console.log("actual position:", this.pos.x, this.pos.y);
+
+            const speed = 5; // pixels/frame
+
+            if (distance > 5) {
+                this.pos.x += (speed * dx) / distance;
+                this.pos.y += (speed * dy) / distance;
+            } else {
+                this.targetPos = null; // Arrivé
+            }
         }
 
         // Appelle la méthode update du parent pour gérer les collisions et autres
